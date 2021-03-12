95f0b0d27e6526b0a74f4af1364ee3e958b93519
kaushalaman97 <hackdroid1997@gmail.com>
2021-03-12 22:43:25 +0530

    update

--- app/value/models.py
+++ app/value/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class sum(models.Model):
     input1=models.CharField(max_length=50);
-    input1=models.CharField(max_length=50);
+    input2=models.CharField(max_length=50);
     result=models.CharField(max_length=50);
--- app/value/views.py
+++ app/value/views.py
@@ -2,45 +2,41 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 from django.core.exceptions import *
-
+from value.models import sum
 def add(request):
+    result=[]
     if request.method == "POST":
         input_1 = request.POST.get('input_1')
         input_2 = request.POST.get('input_2')
 
+        if not request.POST.get('input_1'):
+            return render(request, "base.html")
+
+        if not request.POST.get('input_2'):
+            return render(request, "base.html")
+
         if request.POST.get('add'):
-            result=int(input_1)+int(input_2)
-            result={
-                "result":result
-            }
-            print(result)
-            return render(request, "base.html",result)
+            result.append(int(input_1)+int(input_2))
+
 
         if request.POST.get('Sub'):
-            result=int(input_1)-int(input_2)
-            result={
-                "result":result
-            }
-            print(result)
-            return render(request, "base.html",result)
+            result.append(int(input_1)-int(input_2))
 
 
         if request.POST.get('Mul'):
-            result=int(input_1) *int(input_2)
-            result={
-                "result":result
-            }
-            print(result)
-            return render(request, "base.html",result)
-
+            result.append(int(input_1) *int(input_2))
 
 
         if request.POST.get('Div'):
-            result=int(input_1)/int(input_2)
-            result={
-                "result":result
-            }
-            print(result)
-            return render(request, "base.html",result)
+            result.append(int(input_1)/int(input_2))
+        else:
+            result.append("")
+
+        data=sum.objects.create(input1=input_1,input2=input_2,result=result[0])
+        data.save()
+        result={ "result":result[0]}
+
+
+        return render(request, "base.html",result)
 
     return render(request, "base.html")
